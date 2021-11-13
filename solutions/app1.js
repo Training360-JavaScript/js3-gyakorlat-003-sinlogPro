c4d21b4b5ab2b8b4e26500fd4d676a17e4f7bb2a
Joe <jp.szonyegi@gmail.com>
2021-11-13 10:09:57 +0100

    Solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,11 @@
+const setCookie = (str) => {
+    const now = new Date();
+    now.setTime(now.getTime() + 15 * 60 * 1000);
+    const expires =now.toUTCString;
+       
+    document.cookie = `token=${str}; expires=${expires}`;
+}
 
+
+// export default setCookie;
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,27 @@
+const cookieHandler = {
+    getAll() {
+        const objCookie = {};
+        document.cookie.split('; ')
+            .forEach(item => {
+                objCookie[item.split('=')[0]] = item.split('=')[1];
+            });
+        return objCookie;
+    },
+    toSessionStorage() {
+        document.cookie.split('; ')
+            .forEach(item =>
+                sessionStorage.setItem(item.split('=')[0], item.split('=')[1])
+            );
+    },
+    flush() {
+        document.cookie.split('; ').forEach(x => {
+            document.cookie = `${x.split('=')[0]}=; expires = Thu, 01 Jan 1970 00:00:00 UTC;`
+        })
+    }
+}
 
+
+// export { cookieHandler };
+export {
+    cookieHandler
+};
